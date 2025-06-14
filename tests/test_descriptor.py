81346faadc7380065b0042ad8ecea434183daf2d
Marcel Kröker <kroeker.marcel@gmail.com>
2025-06-14 14:20:32 +0200

    improve tests

--- tests/test_descriptor.py
+++ tests/test_descriptor.py
@@ -1,5 +1,3 @@
-from typing import cast
-
 from statica import Statica
 from statica.core import Field, FieldDescriptor
 
@@ -14,22 +12,24 @@ def test_descriptor() -> None:
 		data_optional: int | None
 		sub: TestSubclass = Field()
 
-	d_data = cast("FieldDescriptor", Test.data)
+	d_data = Test.data
 
+	assert isinstance(d_data, FieldDescriptor)
 	assert d_data.name == "data"
 	assert d_data.owner is Test
 	assert d_data.expected_type is str
 	assert set(d_data.sub_types) == {str}
 	assert d_data.statica_sub_class is None
 
-	d_data_optional = cast("FieldDescriptor", Test.data_optional)
-	assert d_data_optional.name == "data_optional"
+	d_data_optional = Test.data_optional
+	assert isinstance(d_data_optional, FieldDescriptor)
 	assert d_data_optional.owner is Test
 	assert d_data_optional.expected_type == int | None
 	assert set(d_data_optional.sub_types) == {int, type(None)}
 	assert d_data_optional.statica_sub_class is None
 
-	d_sub = cast("FieldDescriptor", Test.sub)
+	d_sub = Test.sub
+	assert isinstance(d_sub, FieldDescriptor)
 	assert d_sub.name == "sub"
 	assert d_sub.owner is Test
 	assert d_sub.expected_type is TestSubclass
